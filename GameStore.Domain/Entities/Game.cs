7b635bf73b34f3b540d6be48543368ed97e5faff
Vlad <vlad.demenity2018@gmail.com>
2020-10-19 20:01:19 +0300

    initgamestore

---
+++ GameStore.Domain/Entities/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Entities
+{
+    public class Game
+    {
+        public int GameId { get; set; }  // id
+        public string Name { get; set; } // Назва гри
+        public string Description { get; set; } // Опис 
+        public string Category { get; set; } // Категорія 
+        public decimal Price { get; set; } // Ціна
+    }
+}
